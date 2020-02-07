5644f7c4c6cf347de7b983d840e143500fea7070
cjn53 <cjn5307@gmail.com>
2020-02-06 20:18:06 -0500

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException, CsvValidationException {
+    // Literally just calls our parser right now (....and is called for tests)
+    CsvParser csvP = new CsvParser("src/Data/bookstore_report2.csv");
+    csvP.printCsv();
+
+  }
+}
